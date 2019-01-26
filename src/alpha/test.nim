1d765a2f1089bc1993c293f622e69c65fd2807ed
Jacob Doyelle Yates <blmvxer@gmail.com>
2019-01-26 15:58:58 -0500

    Create test.nim

---
+++ src/alpha/test.nim
@@ -0,0 +1,15 @@
+let lan = "192.168.0.x"
+var v = 1
+for v in countup(1, 255):
+  let lan = replace(lan, "x", $v)
+  try:
+    let sock = newSocket()
+    sock.connect(lan, Port(80), 500 * 1)
+    echo getHostByName(lan)
+    echo getHostByAddr(lan)
+    sleep(1500)
+    sock.close()
+  except:
+    let ErrorMsg = getCurrentExceptionMsg()
+    echo ErrorMsg
+    continue
