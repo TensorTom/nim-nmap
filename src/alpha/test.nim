b503b28c3036e93e6e4199483a8af19b78322343
Jacob Doyelle Yates <blmvxer@gmail.com>
2019-01-26 18:07:37 -0500

    Update test.nim

--- src/alpha/test.nim
+++ src/alpha/test.nim
@@ -1,17 +1,23 @@
 import nativesockets, net, strutils, os
 
-let localNet = "192.168.0.x"
-var v = 1
-for v in countup(1, 255):
-  let localNet = replace(localNet, "x", $v)
-  try:
-    let sock = newSocket()
-    sock.connect(localNet, Port(80), 500 * 1)
-    echo getHostByName(localNet)
-    echo getHostByAddr(localNet)
-    sleep(1500)
-    sock.close()
-  except:
-    let errorMsg = getCurrentExceptionMsg()
-    echo localNet & ": " & errorMsg
-    continue
+proc localHostNet(): string {.discardable.} =
+  let localNet = "192.168.0.x"
+  var v = 1
+  for v in countup(1, 255):
+    let localNet = replace(localNet, "x", $v)##Run through mask
+    try:
+      let sock = newSocket()
+      sock.connect(localNet, Port(80), 500 * 1)##500 = 1/2 a second
+      echo getHostByName(localNet)
+      echo getHostByAddr(localNet)
+      sleep(1500)
+      sock.close()
+    except:
+      let errorMsg = getCurrentExceptionMsg()
+      echo localNet & ": " & errorMsg
+      continue
+
+proc findLocalAddr(): string {.discardable.} =
+  let localHost = getHostname()
+  let localAddr = getHostByName(localHost)
+  echo localAddr
