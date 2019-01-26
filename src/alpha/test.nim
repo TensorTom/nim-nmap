d7a69b823204bf5742957f6291a65ea48f09e2a8
Jacob Doyelle Yates <blmvxer@gmail.com>
2019-01-26 16:04:59 -0500

    Update test.nim

--- src/alpha/test.nim
+++ src/alpha/test.nim
@@ -1,17 +1,17 @@
 import nativesockets, net, strutils, os
 
-let lan = "192.168.0.x"
+let localNet = "192.168.0.x"
 var v = 1
 for v in countup(1, 255):
-  let lan = replace(lan, "x", $v)
+  let localNet = replace(localNet, "x", $v)
   try:
     let sock = newSocket()
-    sock.connect(lan, Port(80), 500 * 1)
-    echo getHostByName(lan)
-    echo getHostByAddr(lan)
+    sock.connect(localNet, Port(80), 500 * 1)
+    echo getHostByName(localNet)
+    echo getHostByAddr(localNet)
     sleep(1500)
     sock.close()
   except:
-    let ErrorMsg = getCurrentExceptionMsg()
-    echo ErrorMsg
+    let errorMsg = getCurrentExceptionMsg()
+    echo localNet & errorMsg
     continue
