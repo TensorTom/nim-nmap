75430b5014faf0afa5cb78b289e689d743f30de5
Jacob Doyelle Yates <blmvxer@gmail.com>
2019-02-12 18:58:20 -0500

    Update test.nim

--- src/alpha/test.nim
+++ src/alpha/test.nim
@@ -1,23 +1,28 @@
 import nativesockets, net, strutils, os
 
-proc localHostNet(): string {.discardable.} =
-  let localNet = "192.168.0.x"
-  var v = 1
-  for v in countup(1, 255):
-    let localNet = replace(localNet, "x", $v)##Run through mask
-    try:
-      let sock = newSocket()
-      sock.connect(localNet, Port(80), 500 * 1)##500 = 1/2 a second
-      echo getHostByName(localNet)
-      echo getHostByAddr(localNet)
-      sleep(1500)
-      sock.close()
-    except:
-      let errorMsg = getCurrentExceptionMsg()
-      echo localNet & ": " & errorMsg
-      continue
 
-proc findLocalAddr(): string {.discardable.} =
-  let localHost = getHostname()
-  let localAddr = getHostByName(localHost)
-  echo localAddr
+proc main(): string {.discardable.} =
+  var testPort = Port(7612)
+
+  proc findLocalAddr(): string {.discardable.} =
+    let localNet = "192.168.0.x"
+    var v = 1
+    for v in countup(1, 255):
+      let localNet = replace(localNet, "x", $v)##Run through mask
+      try:
+        let sock = newSocket()
+        sock.connect(localNet, testPort, 50 * 1)##500 = 1/2 a second
+        echo getHostByName(localNet)
+        echo getHostByAddr(localNet)
+        sleep(1500)
+        sock.close()
+        quit()
+      except:
+        continue
+
+
+  var socket = newSocket()
+  socket.bindAddr(testPort)
+  socket.listen()
+  findLocalAddr()
+main()
